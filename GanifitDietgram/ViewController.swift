2dac19fc6019106cbc008803353a837dd52474ae
sjkim <7577ksj@nuribom.com>
2021-02-05 08:05:14 +0900

    Create Project

---
+++ GanifitDietgram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GanifitDietgram
+//
+//  Created by 성준 on 2021/02/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
